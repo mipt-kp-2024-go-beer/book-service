638a4ae18d47d5616c946ae85bba4831522f36f9
dfrolov <dfrolov@aq.ru>
2024-11-17 13:53:19 +0300

    First implementation for 'book' and 'stock' structures

---
+++ internal/library/stock.go
@@ -0,0 +1,24 @@
+package library
+
+import "context"
+
+// Stock represents the stock record of a book
+type Stock struct {
+	BookID         string
+	TotalStock     int
+	LentStock      int
+	AvailableStock int
+}
+
+// StockService defines the interface for interacting with stock (business logic)
+type StockService interface {
+	GetStock(ctx context.Context, bookID string) (*Stock, error)
+	ChangeStock(ctx context.Context, bookID string, delta int) error
+}
+
+// StockStore defines the interface for database interactions related to stock
+type StockStore interface {
+	LoadStock(ctx context.Context, bookID string) (*Stock, error)
+	SaveStock(ctx context.Context, stock Stock) error
+	UpdateStock(ctx context.Context, bookID string, delta int) error
+}
